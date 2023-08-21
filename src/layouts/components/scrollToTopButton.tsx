766b8a9c8e57f1f0bb6cae71f46ca287dc649d83
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2023-08-21 18:05:44 +0700

    commit

--- src/layouts/components/scrollToTopButton.tsx
+++ src/layouts/components/scrollToTopButton.tsx
@@ -1,28 +1,48 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 export default function ScrollToTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
   return (
-    <button
-      type="button"
-      data-te-ripple-init
-      data-te-ripple-color="light"
-      className="!fixed bottom-5 right-5 hidden rounded-full bg-red-600 p-3 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg"
-      id="btn-back-to-top"
-    >
-      <svg
-        aria-hidden="true"
-        focusable="false"
-        data-prefix="fas"
-        className="h-4 w-4"
-        role="img"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 448 512"
+    <div className="fixed bottom-2 right-2">
+      <button
+        type="button"
+        onClick={scrollToTop}
+        className={classNames(
+          isVisible ? "opacity-100" : "opacity-0",
+          "bg-pink-600 hover:bg-pink-700 focus:ring-pink-500 inline-flex items-center rounded-full p-3 text-white shadow-sm transition-opacity focus:outline-none focus:ring-2 focus:ring-offset-2",
+        )}
       >
-        <path
-          fill="currentColor"
-          d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"
-        ></path>
-      </svg>
-    </button>
+        <div className="h-0 w-0 border-x-8 border-x-transparent border-b-[16px] border-b-blue-600"></div>
+      </button>
+    </div>
   );
 }
+export const classNames = (...classes) => {
+  return classes.filter(Boolean).join(" ");
+};

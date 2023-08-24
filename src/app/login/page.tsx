9a42f0fc97ddf2e0b20515a209f042d9bd3d39d9
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2023-08-24 13:36:52 +0700

    commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -63,16 +63,6 @@ const Contact = () => {
           style={{ height: "60vh" }}
           className="flex flex-col items-center px-6 mx-auto  lg:py-0"
         >
-          <a
-            href="#"
-            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-          >
-            <img
-              className="w-8 h-8 mr-2"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-              alt="logo"
-            />
-          </a>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">

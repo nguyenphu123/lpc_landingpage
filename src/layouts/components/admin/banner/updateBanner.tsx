75e8e592eb34940c62916b341806a90421f065be
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2023-08-28 09:00:54 +0700

    commit

---
+++ src/layouts/components/admin/banner/updateBanner.tsx
@@ -0,0 +1,164 @@
+"use client";
+
+import React, { useState } from "react";
+
+import { useForm } from "@mantine/form";
+
+import Image from "next/image";
+
+import { updateBanner } from "@/lib/updateData";
+
+import { randomId } from "@mantine/hooks";
+
+import { TextInput, Button, Box, Code, Grid, Col } from "@mantine/core";
+
+const UpdateBanner = ({ banner }) => {
+  const [selectedImage, setSelectedImage] = useState(banner.image);
+
+  const [imagePreview, setImagePreview] = useState("");
+
+  const [successMessage, setSuccessMessage] = useState<string | null>(null); // Updated type declaration
+
+  const onImageChange = (e) => {
+    const file = e.target.files[0];
+
+    setSelectedImage(file);
+
+    const imageUrl = URL.createObjectURL(file);
+
+    setImagePreview(imageUrl);
+  };
+
+  const form = useForm({
+    initialValues: banner,
+  });
+
+  const onSubmitForm = async (values) => {
+    if (selectedImage) {
+      // Nếu có hình ảnh được chọn, tải lên trước
+
+      const formData = new FormData();
+
+      formData.append("file", selectedImage);
+
+      formData.append("upload_preset", "ml_default");
+
+      try {
+        const response = await fetch(
+          "https://api.cloudinary.com/v1_1/derjssgq9/image/upload",
+
+          {
+            method: "POST",
+
+            body: formData,
+          },
+        );
+
+        const data = await response.json();
+
+        values.image = data.secure_url; // Lưu URL hình ảnh đã tải lên vào dữ liệu biểu mẫu
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    // Tiếp tục với phần còn lại của quá trình gửi biểu mẫu
+
+    updateBanner(values);
+
+    form.reset();
+
+    setSuccessMessage("Data added successfully!");
+
+    setTimeout(() => {
+      setSuccessMessage(null);
+    }, 5000);
+  };
+
+  return (
+    // <div style={{ maxHeight: "500px", overflowY: "auto" }}>
+
+    <div className="container">
+      <Box maw={"75%"} mx="auto">
+        <form onSubmit={form.onSubmit((values) => onSubmitForm(values))}>
+          <h3 className="flex justify-center">Update content</h3>
+
+          {/* Hiển thị xem trước ảnh */}
+
+          {imagePreview && (
+            <div className="flex justify-center">
+              <Image
+                src={imagePreview}
+                alt="Preview"
+                width="350"
+                height="350"
+              />
+            </div>
+          )}
+
+          <Grid gutter="lg">
+            <Col span={6}>
+              <TextInput
+                label="Title"
+                placeholder="Title"
+                {...form.getInputProps("title")}
+              />
+            </Col>
+
+            <Col span={6}>
+              <TextInput
+                label="Title (English)"
+                placeholder="Title (English)"
+                {...form.getInputProps("titleEn")}
+              />
+            </Col>
+
+            <Col span={6}>
+              <TextInput
+                label="Content"
+                placeholder="Content"
+                mt="md"
+                {...form.getInputProps("content")}
+              />
+            </Col>
+
+            <Col span={6}>
+              <TextInput
+                label="Content (English)"
+                placeholder="Content (English)"
+                mt="md"
+                {...form.getInputProps("contentEn")}
+              />
+            </Col>
+
+            <Col span={6}>
+              <input type="file" accept="image/*" onChange={onImageChange} />
+            </Col>
+
+            <Col span={6} className="flex justify-end mt-6">
+              {/* Thêm class CSS để đặt nút submit ở góc phải */}
+
+              <Button
+                type="submit"
+                style={{ backgroundColor: "blue" }}
+                size="md"
+              >
+                Submit
+              </Button>
+            </Col>
+          </Grid>
+        </form>
+
+        {successMessage && (
+          <div style={{ marginTop: "16px", color: "green" }}>
+            {successMessage}
+          </div>
+        )}
+      </Box>
+    </div>
+
+    // </div>
+  );
+};
+
+export default UpdateBanner;
---
+++ src/layouts/components/admin/customers/updateCustomer.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import React, { useState } from "react";
+
+import { useForm } from "@mantine/form";
+
+import { TextInput, Button, Box, Code, Grid, Col } from "@mantine/core";
+
+import { updateCustomer } from "@/lib/updateData";
+
+function UpdateCustomer({ Customer }) {
+  const [selectedImage, setSelectedImage] = useState(Customer.src);
+
+  const [successMessage, setSuccessMessage] = useState<string | null>(null); // Updated type declaration
+
+  const form = useForm({
+    initialValues: Customer,
+  });
+
+  const onImageChange = (e) => {
+    const file = e.target.files[0];
+
+    setSelectedImage(file);
+  };
+
+  const onSubmitForm = async (values) => {
+    if (selectedImage) {
+      const formData = new FormData();
+
+      formData.append("file", selectedImage);
+
+      formData.append("upload_preset", "ml_default");
+
+      try {
+        const response = await fetch(
+          "https://api.cloudinary.com/v1_1/derjssgq9/image/upload",
+
+          {
+            method: "POST",
+
+            body: formData,
+          },
+        );
+
+        const data = await response.json();
+
+        values.src = data.secure_url; // Save the uploaded image URL to the form data
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    // Continue with the rest of the form submission
+
+    updateCustomer(values);
+
+    form.reset();
+
+    setSuccessMessage("Data added successfully!");
+
+    setTimeout(() => {
+      setSuccessMessage(null);
+    }, 5000);
+  };
+
+  return (
+    // <div style={{ maxHeight: "500px", overflowY: "auto" }}>
+
+    <div className="container">
+      <Box maw={"75%"} mx="auto">
+        <form onSubmit={form.onSubmit((values) => onSubmitForm(values))}>
+          <h3 className="flex justify-center">Update customers</h3>
+
+          <Grid gutter="lg">
+            <Col span={12}>
+              <TextInput
+                label="Name"
+                placeholder="Name"
+                {...form.getInputProps("name")}
+              />
+            </Col>
+
+            <Col span={6}>
+              <input type="file" accept="image/*" onChange={onImageChange} />
+            </Col>
+
+            <Col span={6} className="flex justify-end mt-6">
+              {/* Thêm class CSS để đặt nút submit ở góc phải */}
+
+              <Button
+                type="submit"
+                style={{ backgroundColor: "blue" }}
+                size="md"
+              >
+                Submit
+              </Button>
+            </Col>
+          </Grid>
+        </form>
+
+        {successMessage && (
+          <div style={{ marginTop: "16px", color: "green" }}>
+            {successMessage}
+          </div>
+        )}
+      </Box>
+    </div>
+
+    // </div>
+  );
+}
+
+export default UpdateCustomer;

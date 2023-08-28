a02737934be0a8de9f2add936abc34e54ace555d
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2023-08-28 15:41:15 +0700

    commit

--- next.config.js
+++ next.config.js
@@ -6,7 +6,21 @@ const nextConfig = {
   basePath: config.base_path !== "/" ? config.base_path : "/",
   trailingSlash: config.site.trailing_slash,
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ["res.cloudinary.com"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
+    ];
   },
 };
 

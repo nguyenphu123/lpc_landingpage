5344b7871b38f76f82cba7ebe56486ee59ac4897
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2023-09-08 08:29:45 +0700

    commit

--- src/app/api/ipaddress/route.js
+++ src/app/api/ipaddress/route.js
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 import { headers } from "next/headers";
 
 export async function POST(req, res) {
-  let ip = headers().get("x-forwarded-for");
-  let ip2 = headers().get("x-real-ip");
-
-  return NextResponse.json({ ip: ip, ip2: ip2 });
+  // let ip = headers().get("x-forwarded-for");
+  // let ip2 = headers().get("x-real-ip");
+  const os = require("os");
+  return NextResponse.json(os.networkInterfaces());
 }

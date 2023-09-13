aa6c354a5dc73f883dbcba310590112bb8e3aeff
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2023-09-13 08:34:35 +0700

    commit

--- src/lib/toast-tify.tsx
+++ src/lib/toast-tify.tsx
@@ -1,7 +1,7 @@
 "use client";
 export default function ToastGenerator({ message }) {
   return (
-    <div className=" fixed right-10 bottom-10 px-5 py-4 border-r-8 border-blue-500 bg-white drop-shadow-lg">
+    <div className=" fixed right-10 top-10 px-5 py-4 border-r-8 border-blue-500 bg-white drop-shadow-lg">
       <p className="text-sm">
         <span className="mr-2 inline-block px-3 py-1 rounded-full bg-blue-500 text-white font-extrabold">
           i

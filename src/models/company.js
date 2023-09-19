05c9414bb0c014a6335f60ad2ad0b80bd413cec1
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2023-09-19 09:19:08 +0700

    commit

--- src/models/company.js
+++ src/models/company.js
@@ -27,38 +27,6 @@ const companySchema = new Schema({
 
     image: String,
   },
-
-  companyAddress: {
-    address: [
-      {
-        content: String,
-
-        contentEn: String,
-
-        title: String,
-      },
-    ],
-
-    addressLink: {
-      content: String,
-
-      title: String,
-    },
-  },
-
-  companySocialAccount: [
-    {
-      platform: String,
-
-      link: String,
-    },
-  ],
-
-  companyContact: [{ typeOfContact: String, contentOfContact: String }],
-
-  companyWebsite: String,
-
-  companyImage: [{ type: String, src: String }],
 });
 const Company =
   mongoose.models.Company || mongoose.model("Company", companySchema);

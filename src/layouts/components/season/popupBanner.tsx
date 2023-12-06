350ef587f6861e9a1054e7c5a6befe670d095b43
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2023-12-06 14:09:54 +0700

    commit

--- src/layouts/components/season/popupBanner.tsx
+++ src/layouts/components/season/popupBanner.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Snowfall from "react-snowfall";
+import "@/styles/closebutton.scss";
 const Popup = ({ children }) => {
   const [isOpen, setIsOpen] = useState(true);
 
@@ -10,14 +11,16 @@ const Popup = ({ children }) => {
   return (
     <div className="z-50">
       {isOpen && (
-        <div className="bg-black bg-opacity-50 z-50 fixed top-0 left-0 w-full h-full flex items-center justify-center">
-          <div className=" p-8 rounded ">
-            <div className="flex justify-end">
+        <div className="  bg-black bg-opacity-50 z-50 fixed top-0 left-0 w-full h-full flex items-center justify-center">
+          <div className=" p-8 rounded Relative  ">
+            <div className="flex justify-end ">
               <button
-                className="bg-slate-400 text-white px-4 py-2 mt-10 flag-icon-rounded"
+                type="button"
                 onClick={togglePopup}
+                className="btn-close absolute top-40 right-auto bg-blue-500 text-white px-4 py-2"
               >
-                X
+                <span className="icon-cross"></span>
+                <span className="visually-hidden">Close</span>
               </button>
             </div>
             {children}

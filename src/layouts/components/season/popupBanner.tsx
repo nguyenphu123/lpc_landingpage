7e2339107878962a71dc484dbd98f9358de65532
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2023-12-25 15:37:17 +0700

    commit

--- src/layouts/components/season/popupBanner.tsx
+++ src/layouts/components/season/popupBanner.tsx
@@ -31,37 +31,34 @@ const Popup = ({ children }) => {
   return (
     <div className="z-50 overflow-hidden w-full">
       {isOpen && (
-        <>
-          <Firework></Firework>
-          <div className=" bg-black bg-opacity-50 z-50 fixed top-0 left-0 w-full h-full flex items-center justify-center">
-            <div className=" p-8 rounded Relative  " ref={targetRef}>
-              <div className="flex justify-end ">
-                <button
-                  type="button"
-                  onClick={togglePopup}
-                  className="btn-close absolute hover:text-cyan-400 top-auto right-auto px-4 py-2"
-                >
-                  <span className="icon-cross"></span>
-                  <span className="visually-hidden">Close</span>
-                </button>
-              </div>
-              {children}
+        <div className=" bg-black bg-opacity-50 z-50 fixed top-0 left-0 w-full h-full flex items-center justify-center">
+          <div className=" p-8 rounded Relative  " ref={targetRef}>
+            <div className="flex justify-end ">
+              <button
+                type="button"
+                onClick={togglePopup}
+                className="btn-close absolute hover:text-cyan-400 top-auto right-auto px-4 py-2"
+              >
+                <span className="icon-cross"></span>
+                <span className="visually-hidden">Close</span>
+              </button>
             </div>
+            {children}
           </div>
-        </>
+        </div>
       )}
-      {/* {isOpen && (
-
-        <Snowfall //for winter only
-          color="#fff"
-          style={{
-            position: "fixed",
-            width: "100vw",
-            height: "100vh",
-            zIndex: 1000,
-          }}
-        />
-      )} */}
+      {isOpen && (
+        <Firework></Firework>
+        // <Snowfall //for winter only
+        //   color="#fff"
+        //   style={{
+        //     position: "fixed",
+        //     width: "100vw",
+        //     height: "100vh",
+        //     zIndex: 1000,
+        //   }}
+        // />
+      )}
     </div>
   );
 };
